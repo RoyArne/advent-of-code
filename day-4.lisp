c44df4ed393b1d45a24f428b7203a55334bdac2b
Roy Arne Gangstad <roy.gangstad@gmail.com>
2022-12-04 17:12:33 +0100

    Use cl-ppcre for input parsing and simplify the range tests.

--- day-4.lisp
+++ day-4.lisp
@@ -1,45 +1,37 @@
 
 (cl:in-package #:advent-of-code)
 
-(defun parse-range (line &optional (start 0) end)
-  (let ((mid (position #\- line :test #'char= :start start :end end)))
-    (cons (parse-integer line :start start :end mid)
-          (parse-integer line :start (1+ mid) :end end))))
-
-(defun parse-assignment-pair (line)
-  (let ((mid (position #\, line :test #'char=)))
-    (list (parse-range line 0 mid)
-          (parse-range line (1+ mid)))))
-        
 (defun read-assignment-pairs ()
   (with-open-input (stream "day-4")
     (loop for line = (read-line stream nil nil)
           while line
-          collect (parse-assignment-pair line))))
+          collect (cl-ppcre:register-groups-bind ((#'parse-integer start1 end1 start2 end2))
+	              ("(\\d+)-(\\d+),(\\d+)-(\\d+)" line)
+                    (list start1 end1 start2 end2)))))
 
-(defun contained-range-p (range1 range2)
-  (and (<= (first range1) (first range2))
-       (>= (rest range1) (rest range2))))
-
-(defun fully-contained-range-pair-p (range1 range2)
-  (or (contained-range-p range1 range2)
-      (contained-range-p range2 range1)))
+(defun containsp (start1 end1 start2 end2)
+  (and (<= start1 start2)
+       (>= end1 end2)))
+  
+(defun fully-contains-p (assignment-pair)
+  (destructuring-bind (start1 end1 start2 end2)
+      assignment-pair
+    (or (containsp start1 end1 start2 end2)
+        (containsp start2 end2 start1 end1))))
 
 (defun count-fully-contained-pairs ()
   "In how many assignment pairs does one range fully contain the other?"
-  (loop for pair in (read-assignment-pairs)
-        count (fully-contained-range-pair-p (first pair) (second pair))))
+  (count-if #'fully-contains-p (read-assignment-pairs)))
 
-(defun in-range-p (n range)
-  (<= (first range) n (rest range)))
+(defun overlapsp (start1 end1 start2 end2)
+  (and (<= start1 end2)
+       (<= start2 end1)))
 
-(defun overlapping-range-pair-p (range1 range2)
-  (or (in-range-p (first range2) range1)
-      (in-range-p (rest range2) range1)
-      (in-range-p (first range1) range2)
-      (in-range-p (rest range1) range2)))
+(defun pair-overlaps-p (assignment-pair)
+  (destructuring-bind (start1 end1 start2 end2)
+      assignment-pair
+    (overlapsp start1 end1 start2 end2)))
 
 (defun count-overlapping-pairs ()
   "In how many assignment pairs do the ranges overlap?"
-  (loop for pair in (read-assignment-pairs)
-        count (overlapping-range-pair-p (first pair) (second pair))))
+  (count-if #'pair-overlaps-p (read-assignment-pairs)))
