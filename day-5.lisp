0e82b64d76d302f7c5114a0cd978f84792b1cffd
Roy Arne Gangstad <roy.gangstad@gmail.com>
2022-12-05 21:43:44 +0100

    Cleaned up instruction execution and input reader.
    Moved a call to reverse from READ-STACKS into MAKE-STACK-VECTORS.
    
    Renamed the move crates function for part 1 from MOVE-CRATES to
    MOVE-CRATES-1-BY-1.
    
    Added a move crates function for part 2 named MOVE-MULTIPLE-CRATES.
    
    Updated EXECUTE-INSTRUCTIONS to take a move crates function as a
    parameter.

--- day-5.lisp
+++ day-5.lisp
@@ -2,10 +2,10 @@
 (cl:in-package #:advent-of-code)
 
 (defun read-stacks (stream)
-  (reverse (loop for line = (read-line stream nil nil)
-                 while (find #\[ line :test #'char=)
-                 collect (loop for i from 1 below (length line) by 4
-                               collect (char line i)))))
+  (loop for line = (read-line stream nil nil)
+        while (find #\[ line :test #'char=)
+        collect (loop for i from 1 below (length line) by 4
+                      collect (char line i))))
 
 (defun make-empty-stacks (line)
   (map 'vector
@@ -16,7 +16,7 @@
 
 (defun make-stack-vectors (lines)
   (loop with stacks = (make-empty-stacks (first lines))
-        for level in lines
+        for level in (reverse lines)
         do (loop for c in level
                  for stack across stacks
                  unless (char= #\Space c)
@@ -35,45 +35,42 @@
                   ;; 1- so they can be used as array indices.
                   (list n (1- from) (1- to)))))
 
-(defun read-puzzle-data-for-part-1 ()
+(defun read-stacks-and-instructions ()
   (with-open-input (stream "day-5")
     (let ((stacks (make-stack-vectors (read-stacks stream))))
       (read-line stream)
       (values stacks
               (read-instructions stream)))))
 
-(defun move-crates (n from to)
+(defun move-crates-1-by-1 (n from to)
   (loop for i from 1 upto n
         do (vector-push (vector-pop from) to)))
 
-(defun execute-instructions (stacks instructions)
+(defun execute-instructions (stacks instructions move-function)
   (loop for instruction in instructions
         do (destructuring-bind (n from to)
                instruction
-             (move-crates n (aref stacks from) (aref stacks to)))))
+             (funcall move-function n (aref stacks from) (aref stacks to)))))
 
 (defun rearrange-crates ()
   "After the rearrangement procedure completes, what crate ends up on top of
 each stack?"
   (multiple-value-bind (stacks instructions)
-      (read-puzzle-data-for-part-1)
-    (execute-instructions stacks instructions)
+      (read-stacks-and-instructions)
+    (execute-instructions stacks instructions #'move-crates-1-by-1)
     (print-top-crates stacks)))
 
-(defun execute-instructions-without-reordering (stacks instructions)
-  (loop for instruction in instructions
-        do (destructuring-bind (n from to)
-               instruction
-             (let ((to-stack (aref stacks to)))
-               (move-crates n (aref stacks from) to-stack)
-               (let ((start (- (length to-stack) n)))
-                 (setf (subseq to-stack start) (reverse (subseq to-stack start))))))))
-  
+(defun move-multiple-crates (n from to)
+  (let ((start (length to)))
+    (incf (fill-pointer to) n)
+    (setf (subseq to start) (subseq from (- (length from) n)))
+    (decf (fill-pointer from) n)))
+
 (defun rearrange-crates-without-reordering ()
   "After the rearrangement procedure completes, what crate ends up on top of
 each stack?"
   (multiple-value-bind (stacks instructions)
-      (read-puzzle-data-for-part-1)
-    (execute-instructions-without-reordering stacks instructions)
+      (read-stacks-and-instructions)
+    (execute-instructions stacks instructions #'move-multiple-crates)
     (print-top-crates stacks)))
     
