3a1b89ce40ce429f87813b680a62b66599de32be
Roy Arne Gangstad <roy.gangstad@gmail.com>
2022-12-09 19:17:25 +0100

    Complex numbers exist!
    Greatly simplified code by representing positions and movements
    with complex numbers.

--- 2022/day-9.lisp
+++ 2022/day-9.lisp
@@ -19,86 +19,52 @@ See also"))
 (cl:in-package #:aoc-2022-day-9)
 
 (defun read-motions (stream)
-  "Return a list of motions in the form \(<direction> <distance>\)."
   (loop for line = (read-line stream nil nil)
         while line
         collect (list (ecase (char line 0)
-                        (#\U :up)
-                        (#\R :right)
-                        (#\D :down)
-                        (#\L :left))
+                        (#\U #c(0 1))
+                        (#\R #c(1 0))
+                        (#\D #c(0 -1))
+                        (#\L #c(-1 0)))
                       (parse-integer (subseq line 2)))))
 
-(defun position= (position1 position2)
-  (and (= (first position1) (first position2))
-       (= (rest position1) (rest position2))))
+(defun sign (distance)
+  (cond
+    ((minusp distance)
+     1)
+    ((plusp distance)
+     -1)
+    (t
+     0)))
 
-(defun move (position direction)
-  (ecase direction
-    (:up (cons (first position) (incf (rest position))))
-    (:right (cons (incf (first position)) (rest position)))
-    (:down (cons (first position) (decf (rest position))))
-    (:left (cons (decf (first position)) (rest position)))))
+(defun adjust-tail (tail head)
+  (let ((distance (- tail head)))
+    (if (or (> (abs (realpart distance)) 1)
+            (> (abs (imagpart distance)) 1))
+        (+ tail
+           (complex (* 1 (sign (realpart distance)))
+                    (* 1 (sign (imagpart distance)))))
+        tail)))
 
-(defun distance (coordinate1 coordinate2)
-  (let* ((d (- coordinate2 coordinate1)))
-    (values (abs d)
-            (cond
-              ((plusp d) 1)
-              ((minusp d) -1)
-              (t 0)))))
-
-(defun update (tail head)
-  (multiple-value-bind (dx sx)
-      (distance (first tail) (first head))
-    (multiple-value-bind (dy sy)
-        (distance (rest tail) (rest head))
-      (cons (+ (first tail)
-               (if (zerop dx)
-                   0
-                   (case dy
-                     ((0 1) (* (1- dx) sx))
-                     (2 (* 1 sx)))))
-            (+ (rest tail)
-               (if (zerop dy)
-                   0
-                   (case dx
-                     ((0 1) (* (1- dy) sy))
-                     (2 (* 1 sy)))))))))
-          
-(defun simulate-motions (motions)
-  (loop with head = (cons 0 0) ; (cons <horizontal> <vertical>)
-        with tail = (cons 0 0)
-        with tail-positions = (list tail)
+(defun simulate-motions (motions n)
+  (loop with knots = (make-array n :initial-element #c(0 0))
+        with tail-positions = (list (aref knots (1- n)))
         for motion in motions
         do (destructuring-bind (direction distance)
                motion
              (loop repeat distance
-                   do (setf head (move head direction)
-                            tail (update tail head))
-                   do (push tail tail-positions)))
+                   do (incf (aref knots 0) direction)
+                   do (loop for i from 1 below n
+                            do (setf (aref knots i) (adjust-tail (aref knots i)
+                                                                 (aref knots (1- i))))
+                            finally (pushnew (aref knots (1- n)) tail-positions :test #'=))))
         finally (return tail-positions)))
 
-(defun simulate-motions-on-larger-rope (motions)
-  (loop with n = 10
-        with knots = (loop repeat n collect (cons 0 0))
-        with tail-positions = (list (nth (1- n) knots))
-        for motion in motions
-        do (destructuring-bind (direction distance)
-               motion
-             (loop repeat distance
-                   do (setf (nth 0 knots) (move (nth 0 knots) direction))
-                   do (loop for tail from 1 upto (- n 1)
-                            do (setf (nth tail knots) (update (nth tail knots)
-                                                              (nth (1- tail) knots)))
-                            do (push (nth (1- n) knots) tail-positions))))
-        finally (return tail-positions)))
+(defun how-many-positions-does-the-tail-of-the-rope-visit-at-least-once?
 
-(defun how-many-positions-does-the-tail-of-the-rope-visit-at-least-once? (part-number
-                                                                          &optional filename)
+    (part-number &optional filename)
+
   (with-open-input (stream (or filename "day-9"))
     (ecase part-number
-      (1 (length (remove-duplicates (simulate-motions (read-motions stream))
-                                    :test #'position=)))
-      (2 (length (remove-duplicates (simulate-motions-on-larger-rope (read-motions stream))
-                                    :test #'position=))))))
+      (1 (length (simulate-motions (read-motions stream) 2)))
+      (2 (length (simulate-motions (read-motions stream) 10))))))
